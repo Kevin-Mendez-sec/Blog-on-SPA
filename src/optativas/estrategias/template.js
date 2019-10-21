dcc86db5f03124a8e83c81e6afc4522c40c40af2
Kevin Mendez <kevinchemy060202@gmail.com>
2019-10-20 22:57:35 -0500

    vamooos...

--- src/optativas/estrategias/template.js
+++ src/optativas/estrategias/template.js
@@ -3,10 +3,11 @@ var yo =require('yo-yo');
 module.exports = yo`
 <div>
   <h1 class="blog-post-title text-center">ESTRATEGIAS</h1>
-    <h2>Mision</h2>
-    <p class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima totam voluptatum possimus tempore inventore quam vitae eaque beatae tenetur ab repellat dicta dolorum rem nam, vel iusto voluptatem impedit ratione! Doloremque architecto aliquam reprehenderit explicabo tempora at distinctio eligendi voluptatem laboriosam dolorum vel voluptatum nihil, sint nemo quibusdam laborum asperiores a veritatis aperiam quaerat laudantium, repudiandae quia. Facere, aliquid debitis!   
-    <h2>Vision</h2>
-    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam obcaecati consequuntur laborum quae nostrum recusandae, ea perferendis placeat veniam, est rem expedita cupiditate eaque facere itaque esse. Exercitationem a dolor quis, laudantium odio recusandae veniam quas fugit ab necessitatibus tenetur sunt aliquam minima aspernatur repellat quo, laboriosam facilis iusto quibusdam officiis! Corrupti debitis pariatur sapiente! Commodi dignissimos ea pariatur natus!
-    <h2>Metas</h2>
-    <p class="text-justify">Lorem, ipsum dolor <br> sit amet consectetur <br> adipisicing elit. Quia, eligendi.
+    <h2 class="blog-post-title text-center"> Semáforos Inteligentes </h2>
+    <h3>Visión</h3>
+    <div class="text-justify">Nuestro prototipo consiste en un semáforo inteligente, el cual será alimentado con una celda solar y con él se busca mejorar los intervalos de tiempo de espera: siga, precaución y pare, esto se quiere lograr automatizando y programando las decisiones que tomaría un policía de tránsito cuando un semáforo no funciona. </div>
+    <h3>Mision</h3>
+    <div class="text-justify">Queremos desarrollar este prototipo ya que es una forma sustentable para resolver el contratiempo que causa el tráfico, es un proyecto verde o ecológico porque como lo dice su definición un proyecto ecológico es aquel que está orientado al desarrollo de prácticas responsables con su entorno y con el diseño de soluciones. En este proyecto se busca disminuir la cantidad de contaminación producida por los automóviles y esto se logrará de una forma amigable con el ambiente.</div>
+    <h3>Metas</h3>
+    <div class="text-justify">Se lograría la reducción de tiempos que se emplean en los recorridos en vehículo,  la contaminación ambiental, auditiva y visual, lo cual se pretende lograr a finales de este semestre.</div>
 </div>`;
